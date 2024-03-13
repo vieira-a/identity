1c80fd97621e20e4c381b718dfc322a2ab0e38ff
Anderson Vieira <asvieira.br@gmail.com>
2024-03-12 23:40:28 -0300

    feat(group): add application layer with read by id db usecase and service

--- src/application/user-group/usecases/db-user-group.usecase.ts
+++ src/application/user-group/usecases/db-user-group.usecase.ts
@@ -2,11 +2,16 @@ import { UserGroupDto } from 'src/domain/user-group/dtos';
 
 import {
   CreateUserGroup,
+  ReadUserGroupById,
   ReadUserGroups,
 } from '../../../domain/user-group/usecases';
-import { CreateUserGroupInput } from '../inputs/create-user-group.input';
+import { CreateUserGroupInput } from '../inputs';
 
-export interface DbUserGroup extends CreateUserGroup, ReadUserGroups {
+export interface DbUserGroup
+  extends CreateUserGroup,
+    ReadUserGroupById,
+    ReadUserGroups {
   create: (data: CreateUserGroupInput) => Promise<CreateUserGroupInput>;
+  readById: (guid: string) => Promise<UserGroupDto>;
   readAll: () => Promise<UserGroupDto[]>;
 }
